51621fd6126e1545ce4aa58ac9898c1dc1915338
akshay <akshay.k1738@gmail.com>
2021-08-07 18:23:46 -0500

    Init Push Service

---
+++ push-service/runner.cc
@@ -0,0 +1,64 @@
+#include <chrono>
+#include <thread>
+#include <vector>
+
+#include "glog/logging.h"
+#include "runner.h"
+
+#include "absl/base/attributes.h"
+#include "absl/flags/flag.h"
+#include "absl/strings/str_format.h"
+
+ABSL_FLAG(uint64_t, cycle_time_minutes, 1,
+          "Set the time for which the Push Service cycle repeats");
+
+namespace dasdd {
+namespace {
+constexpr unsigned int kParallelThreadCount =
+    5; // Upto std::thread::hardware_concurrency() limit
+
+void PrintWelcomeText() {
+  LOG(INFO) << "Running Push Service...";
+  LOG(INFO) << absl::StrFormat("Parallel thread count: %d",
+                               kParallelThreadCount);
+}
+
+void Handler() {
+  LOG(INFO) << "Thread id: " << std::this_thread::get_id();
+
+  // Read the files n' extract the data
+  // read()
+  // Dispatch the data to AWS RDS
+  // OR... buffer the dispatch.. e.g. in a vector so as to avoid several cycles
+  // (will need locking around a std::vector?) dispatch()
+}
+
+ABSL_ATTRIBUTE_NORETURN void Dispatch() {
+  std::vector<std::thread> threads;
+  while (true) {
+    for (unsigned int i = 0; i < kParallelThreadCount; i++) {
+      std::thread t(Handler);
+      threads.push_back(std::move(t));
+    }
+    for (std::thread &t : threads) {
+      t.join();
+    }
+
+    LOG(INFO) << "Cycle finished. Sleeping until next cycle starts...";
+    threads.clear();
+    std::this_thread::sleep_for(
+        std::chrono::minutes(absl::GetFlag(FLAGS_cycle_time_minutes)));
+  }
+}
+
+} // namespace
+
+absl::StatusOr<bool> Run() {
+  PrintWelcomeText();
+
+  Dispatch();
+
+  return absl::OkStatus();
+}
+
+} // namespace dasdd
