c5f3a3b252afd1aa91663d7eefda5fe03054a83b
Akshay <38117036+aksh0001@users.noreply.github.com>
2021-08-11 15:47:45 -0500

    Update workflow_start.sh

--- workflow_start.sh
+++ workflow_start.sh
@@ -7,6 +7,6 @@ usernames=("Allison45555547" "ElizaHahns")
 passwords=("A2IHNDjPu23SNEjfy4ts" "AKJHD97434%^%")
 
 for i in "${!usernames[@]}"; do
-  echo bazel run ${TARGET} -- --bot_username="${usernames["${i}"]}" --bot_password="${passwords["${i}"]}" --bot_output_directory=${BOT_OUT} &
+  echo bazel run ${TARGET} -- --bot_username="${usernames["${i}"]}" --bot_password="${passwords["${i}"]}" --bot_output_directory=${BOT_OUT}
   bazel run ${TARGET} -- --bot_username="${usernames["${i}"]}" --bot_password="${passwords["${i}"]}" --bot_output_directory=${BOT_OUT} &
 done
