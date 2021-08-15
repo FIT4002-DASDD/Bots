9e8d36f7237c923cf009d18925b6f5da01da824a
saratran <utra0001@student.monash.eud>
2021-08-15 21:39:23 +1000

    Update drive config to override user-agent so that headless mode works

--- bot/stages/config.py
+++ bot/stages/config.py
@@ -13,8 +13,8 @@ WEBDRIVER_OPTIONS = [
     '--disable-blink-features=AutomationControlled',
     '--headless',
     '--no-sandbox',
-    '--disable-gpu'
-    'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+    '--disable-gpu',
+    "user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
 ]
 
 
@@ -22,4 +22,5 @@ def create_chromedriver() -> Chrome:
     op = Options()
     for arg in WEBDRIVER_OPTIONS:
         op.add_argument(arg)
-    return Chrome(executable_path=FLAGS.path_to_chromedriver, options=op)
+    driver = Chrome(executable_path=FLAGS.path_to_chromedriver, options=op)
+    return driver
