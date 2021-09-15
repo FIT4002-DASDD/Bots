061149a71987beab07c520318399d9cc5505c87b
saratran <utra0001@student.monash.edu>
2021-09-15 13:33:04 +1000

    stop bazel server after build in start script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,6 +5,7 @@ BASEDIR=$(dirname $0)
 # Build bot binary
 echo "-------Building bot with Bazel-------"
 bazel build //bot:app
+bazel shutdown  # stop bazel server to reduce memory consumption
 
 # Run bot scheduler
 echo "-------Running bot scheduler-------"
